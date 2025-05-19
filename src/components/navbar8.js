1d37d9c4c6427a803fa98db1c40f283525f37e5b
keyurraval <ravalkeyur054@gmail.com>
2025-05-20 00:30:46 +0530

    changes

--- src/components/navbar8.js
+++ src/components/navbar8.js
@@ -52,11 +52,11 @@ const Navbar8 = (props) => {
           <div className="navbar8-buttons1">
           </div>
         </div>
-        <div data-thq="thq-burger-menu" className="navbar8-burger-menu">
+        {/* <div data-thq="thq-burger-menu" className="navbar8-burger-menu">
           <svg viewBox="0 0 1024 1024" className="navbar8-icon14">
             <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
           </svg>
-        </div>
+        </div> */}
         <div data-thq="thq-mobile-menu" className="navbar8-mobile-menu">
           <div className="navbar8-nav">
             <div className="navbar8-top">
@@ -322,3 +322,4 @@ Navbar8.propTypes = {
 }
 
 export default Navbar8
+
